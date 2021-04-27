3fee8286efc61385f358e13e76d3fe37c7084be6
Lucasdvs10 <lucas.verzaro@gmail.com>
2021-04-27 20:42:00 -0300

    Deixei mais claro o que o usuário precisa fazer

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from preverNotas import *
 
 dictNotas = {}
 print('Digite uma nota de 0 a 5 para os filmes a seguir.')
-print(' Caso não tenha visto o filme, apenas aperte enter e te darei a nota que você provalvemente dará para os filmes não vistos')
+print('Caso não tenha visto o filme, apenas aperte enter e te darei a nota que você provalvemente dará para os filmes não vistos')
+print(
+    '------------------------------------------------------------------------------------------------------------------------------')
 for filme in arrayFilmes:
     notaFilme = input('Digite sua nota para o filme %s: ' %filme)
 
@@ -10,4 +12,6 @@ for filme in arrayFilmes:
         dictNotas.update({filme: notaFilme})
 
 resultado = preverNotas(dictNotas)
-print(resultado)
+
+for filme in resultado:
+    print('%s: %.2f' %(filme, resultado.get(filme)))
